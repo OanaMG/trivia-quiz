ea65f83bef5624e62c0d653afb1f22d11a0e5eb1
OanaMG <oana_ghidiiu@yahoo.com>
2021-02-26 18:19:31 +0000

    removing Shuffle functionality from Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,42 +1,23 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import '../App/App.css';
 
-function Button ({arrayIncorrect, itemCorrect, handleCorrectClick, handleIncorrectClick, cleanText}){
+function Button ({answersArray, handleCorrectClick, handleIncorrectClick}){
     const [isDisabled, setDisabled] = useState(false);
-    let answersArray =  [...arrayIncorrect, itemCorrect];
-
-    function shuffle(arr) {
-        let i,j,temp;
-        for (i = arr.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-        }
-        return arr;    
-    };
-
-    console.log(itemCorrect);
- 
-    shuffle(answersArray);
 
     return(
     <div>
-        {answersArray.map ((item, index) => 
-        (<div key={index}>
-            <button disabled={isDisabled} className="answer-button" onClick={ () => {
-                setDisabled(true);
-                let correctAnswer = itemCorrect;
-               
-                if (item === correctAnswer){
-                    handleCorrectClick();
-                } else {
-                    handleIncorrectClick();
-                }
-            }}>{cleanText(item)}</button>
-        </div>)
-         )}
-       
+        {answersArray.map ((item, index) =>
+            (<div key={index}>
+                <button disabled={isDisabled} className="answer-button" onClick={ () => {
+                    setDisabled(true);
+                    if(item.isCorrect){
+                        handleCorrectClick();
+                    } else {
+                        handleIncorrectClick();
+                    }
+                }}>{item.text}</button>
+            </div>)
+        )}
     </div>
     )    
 }
